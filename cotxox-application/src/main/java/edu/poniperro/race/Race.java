1dde7922869befbc74c37c2213d8f3bac412e5cd
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-12 16:04:28 +0100

    feat(Race): add attributes

---
+++ cotxox-application/src/main/java/edu/poniperro/race/Race.java
@@ -0,0 +1,12 @@
+package edu.poniperro.race;
+
+public class Race {
+    private final String creditCard = "";
+    private final String origin = "";
+    private String destination = "";
+    private double distance;
+    private final int expectedTime = 0;
+    private final int raceTime = 0;
+    private final int totalCost = 0;
+    private int tip = 0;
+}
