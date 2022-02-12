5c8e3c3311fbfb53b669f58e413d84992a655736
Ezequiel de la Rosa <erosarosario@cifpfbmoll.eu>
2022-02-12 15:15:24 +0100

    innital setup

---
+++ cotxox-application/src/main/java/edu/poniperro/Cotxox.java
@@ -0,0 +1,13 @@
+package edu.poniperro;
+
+/**
+ * Hello world!
+ *
+ */
+public class Cotxox
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
