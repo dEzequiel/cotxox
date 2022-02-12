0dea51de9f07ba7a5f677e4e5574b4985c5bb63f
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-12 20:42:11 +0100

    feat(PoolDrivers): add method to assign random driver from pool

--- cotxox-application/src/main/java/edu/poniperro/drivers/PoolDrivers.java
+++ cotxox-application/src/main/java/edu/poniperro/drivers/PoolDrivers.java
@@ -2,11 +2,12 @@ package edu.poniperro.drivers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class PoolDrivers {
     
-    private List<Driver> drivers = new ArrayList<>();
-
+    private List<Driver> drivers = new ArrayList<Driver>();
+    
     public PoolDrivers(List<Driver> drivers) {
         this.drivers = drivers;
     }
@@ -15,5 +16,17 @@ public class PoolDrivers {
         return this.drivers;
     }
 
+    public Driver assignDriver() {
+        Driver driver = new Driver();
+        int PoolDriverNumber = ThreadLocalRandom.current().nextInt(getPoolDrivers().size());
 
+        while(driver.isOccupied() == false) {
+            driver = getPoolDrivers().get(PoolDriverNumber); //Getting array driver object
+            if (!driver.isOccupied()) {
+                driver.setOccupied(true);
+            }
+        }
+
+        return driver;
+    }
 }
