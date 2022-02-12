d2c315028e81dfd9d6cb3441f69cf8a31293924b
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-12 16:14:14 +0100

    feat(Driver): create Driver constructor

--- cotxox-application/src/main/java/edu/poniperro/drivers/Driver.java
+++ cotxox-application/src/main/java/edu/poniperro/drivers/Driver.java
@@ -4,10 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Driver {
+
     private final String name;
     private final String model;
     private final String enrollment;
     private double averageRating;
     private boolean isOccupied = false;
     private List<Byte> ratings = new ArrayList<Byte>();
+
+    public Driver(String name, String model, String enrollment) {
+        this.name = name;
+        this.model = model;
+        this.enrollment = enrollment;
+    }
 }
+
+
