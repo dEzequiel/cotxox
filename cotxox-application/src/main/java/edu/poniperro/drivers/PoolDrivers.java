6055847d8978b807b2e22ea26db396a4128dd23c
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-02-12 17:07:52 +0100

    feat(PoolDrivers): add data structure that saves drivers and create constructor

---
+++ cotxox-application/src/main/java/edu/poniperro/drivers/PoolDrivers.java
@@ -0,0 +1,15 @@
+package edu.poniperro.drivers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PoolDrivers {
+    
+    private List<Driver> drivers = new ArrayList<>();
+
+    public PoolDrivers(List<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    
+}
